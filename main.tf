6085c2f674a3e9916531b3fbfd496012bb70dd44
Vladah27 <myawsvh@gmail.com>
2021-06-30 17:15:50 -0400

    Added more local-exec provisioners

--- main.tf
+++ main.tf
@@ -87,6 +87,13 @@ resource "null_resource" "quiz-experts" {
   provisioner "local-exec" {
     command = "echo \"Hello, $(whoami)\""
   }
+  provisioner "local-exec" {
+    command = "date"
+  }
+
+  provisioner "local-exec" {
+    command = "echo The server's IP address is ${self.private_ip}"
+  }
 }
 
 output "server_names_count" {
